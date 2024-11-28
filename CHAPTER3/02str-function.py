e3b473cd9fe94b5992c94c3fe4af996333ea7f73
nawazishfaraz <nawazishfaraz14@gmail.com>
2024-11-28 21:32:37 +0530

    my first commit

---
+++ CHAPTER3/02str-function.py
@@ -0,0 +1,23 @@
+#1: len function
+name = "faraz"
+print(len(name))
+
+#2 : startswith function
+name = "faraz"
+print(name.startswith("fa"))
+
+#3: endswith function
+name = "faraz"
+print(name.endswith("raz"))
+
+#4: capitalize function
+name = "faraz bhai"
+print(name.capitalize())
+
+#5: capitalize each letter from the word
+name = "faraz bro"
+print(name.title())
+
+
+
+
